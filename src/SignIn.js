b084bd9b6d8bb2a9034baa5d5c8e88a2b4a27fe2
antonprilepin <antonprilepin78@gmail.com>
2024-02-15 01:03:18 +0200

    commit

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function SignIn() {
     return (
-      <div>
-        <h1>Sam PIDOR</h1>
+      <header>
+      <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" />
       </div>
+      <div className="navbar-icons">
+        <img src={settingsIcon} alt="Settings" />
+        {/* Сюда нужно будет добавить иконки */}
+      </div>
+    </nav>
+    </header>
     );
   }
   
