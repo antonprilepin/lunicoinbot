a24e57358fa48bf73a32dcdfc160103442a6f09d
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-16 01:09:59 +0200

    minor GUI changes

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,13 +4,12 @@ import './SignIn.css';
 function SignIn() {
     return (
         <div>
-            <div className='container'>
+            <div className='text-container'>
                 <h1 className='title-text'>Вход с помощью <br /> фразы восстановления</h1>
                 <p className='text'>Пожалуйста, введите фразу восстановления, связанную с аккаунтом.</p>
-                <input className='input-pole' oninput="this.setAttribute('placeholder', this.value);" type="text" placeholder='Введите фразу восстановления*'></input>
-                <br />
-                <button className='continue-button'>Далее</button>
             </div>
+            <input className='input-pole' oninput="this.setAttribute('placeholder', this.value);" type="text" placeholder='Введите фразу восстановления*'></input>
+            <button className='continue-button'>Далее</button>
         </div>
 
     );
