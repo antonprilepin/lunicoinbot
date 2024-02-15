e845497c29b451886f4680fd79482f87b2597ebf
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-16 00:56:14 +0200

    check Sign In commit

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './SignIn.css';
 
 function SignIn() {
-
     return (
-        <div>
-            <h1>PETUH</h1>
-        </div>
+      <div>
+        <h1 className='title-text'>Вход с помощью <br/> фразы восстановления</h1>
+        <p className='podtitle-text'>Пожалуйста, введите фразу восстановления, связанную с аккаунтом.</p>
+        <input className='input-pole' oninput="this.setAttribute('placeholder', this.value);" type="text" placeholder='Введите фразу восстановления*'></input>
+        <br/>
+        <button className='continue-button'>Далее</button>
+      </div>
+      
     );
-}
-
-export default SignIn;
+  }
+  
+  export default SignIn;
+  
