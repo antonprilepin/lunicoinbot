3677c0fa0618c2e0e8d5a15f07e8bd9abce63f35
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-15 02:20:31 +0200

    sign in button and text added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const word = '  Кошелёк Telegram нового<br> поколения. Безопасно, <br>быстро и через блокчейн<br>LUNI.    ';
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const timer = setInterval(() => {
+      if (currentIndex < word.length) {
+        setText(prevText => prevText + word[currentIndex]);
+        currentIndex++;
+      } else {
+        clearInterval(timer);
+      }
+    }, 50);
+    return () => clearInterval(timer);
+  }, []);
+
+
   return (
     <div className="bg">
-      <h1>PIDOR</h1>
+      {/* <img src='https://static.herewallet.app/intro.35bf1b5e.png' className='main-photo'></img> */}
+      <div className='main-text-container'>
+        <h1 className='title-text'>Luni Wallet</h1>
+        <h2 dangerouslySetInnerHTML={{ __html: text }} className='text'></h2>
+      </div>
+      <button className="button">Зарегистрироваться</button>
     </div>
   );
 }
