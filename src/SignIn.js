b11e2d7024f44f7f7ec866b5b2daf8e16f7a5b36
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-16 01:04:36 +0200

    minor changes

--- src/SignIn.js
+++ src/SignIn.js
@@ -3,16 +3,17 @@ import './SignIn.css';
 
 function SignIn() {
     return (
-      <div>
-        <h1 className='title-text'>Вход с помощью <br/> фразы восстановления</h1>
-        <p className='podtitle-text'>Пожалуйста, введите фразу восстановления, связанную с аккаунтом.</p>
-        <input className='input-pole' oninput="this.setAttribute('placeholder', this.value);" type="text" placeholder='Введите фразу восстановления*'></input>
-        <br/>
-        <button className='continue-button'>Далее</button>
-      </div>
-      
+        <div>
+            <div className='container'>
+                <h1 className='title-text'>Вход с помощью <br /> фразы восстановления</h1>
+                <p className='podtitle-text'>Пожалуйста, введите фразу восстановления, связанную с аккаунтом.</p>
+                <input className='input-pole' oninput="this.setAttribute('placeholder', this.value);" type="text" placeholder='Введите фразу восстановления*'></input>
+                <br />
+                <button className='continue-button'>Далее</button>
+            </div>
+        </div>
+
     );
-  }
-  
-  export default SignIn;
-  
+}
+
+export default SignIn;
