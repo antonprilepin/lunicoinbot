01c7556ac18aa11ceba4e4d15ebbca2f74e18d07
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-16 00:33:32 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import SignIn from './SignIn';
 
 function App() {
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './SignIn.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function SignIn() {
 
