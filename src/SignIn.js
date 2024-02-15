5f06e40c981d85209ac22e668b89907d84afd304
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-16 01:06:30 +0200

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -6,7 +6,7 @@ function SignIn() {
         <div>
             <div className='container'>
                 <h1 className='title-text'>Вход с помощью <br /> фразы восстановления</h1>
-                <p className='podtitle-text'>Пожалуйста, введите фразу восстановления, связанную с аккаунтом.</p>
+                <p className='text'>Пожалуйста, введите фразу восстановления, связанную с аккаунтом.</p>
                 <input className='input-pole' oninput="this.setAttribute('placeholder', this.value);" type="text" placeholder='Введите фразу восстановления*'></input>
                 <br />
                 <button className='continue-button'>Далее</button>
