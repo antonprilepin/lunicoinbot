ee806a9d9eb5bdd920a751c4ef1bc13ed130acf9
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-02-16 00:32:06 +0200

    minor changes test

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,23 +2,22 @@ import './SignIn.css';
 
 function SignIn() {
     return (
-<<<<<<< Updated upstream
-      <header>
-      <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logo} alt="Logo" />
-      </div>
-      <div className="navbar-icons">
-        <img src={settingsIcon} alt="Settings" />
-        {/* Сюда нужно будет добавить иконки */}
-      </div>
-    </nav>
-    </header>
-=======
-        <div className='bg'>
-            <h1>Sam PIDOR</h1>
+        <div>
+            <header>
+                <nav className="navbar">
+                    <div className="navbar-logo">
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="navbar-icons">
+                        <img src={settingsIcon} alt="Settings" />
+                        {/* Сюда нужно будет добавить иконки */}
+                    </div>
+                </nav>
+            </header>
+            <div className='bg'>
+                <h1>Sam PIDOR</h1>
+            </div>
         </div>
->>>>>>> Stashed changes
     );
 }
 
